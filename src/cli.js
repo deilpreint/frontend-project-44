1e4f528b4155f56415e0c81493c758a5fd03d7c3
deilpreint <losk4000@gmail.com>
2025-05-14 14:09:37 +0300

    Six block

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const greetUser = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+export const getRandomNumber = (min = 1, max = 100) => 
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
 };
 
-export default greetUser;
+export default {
+  getRandomNumber,
+  greeting
+};
