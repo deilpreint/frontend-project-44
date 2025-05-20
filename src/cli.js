9ca8b2a873da468d43011cf23f9ca4f91de6ba7b
Ivan <145787971+deilpreint@users.noreply.github.com>
2025-05-20 08:15:51 +0300

    fix ninth block

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,9 @@
-#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
 export const getRandomNumber = (min = 1, max = 100) => 
   Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const greeting = () => {
-  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
