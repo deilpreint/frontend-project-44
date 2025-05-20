3313f5a58ae5a145a9acc652cc71bc2e690e1b50
deilpreint <losk4000@gmail.com>
2025-05-20 12:57:58 +0300

    test

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    rules: {
+      'semi': 'off',
+      'quotes': 'off',
+      'no-trailing-spaces': 'off',
+      'eol-last': 'off'
+    }
+  }
+]
